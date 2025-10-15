96bd0ed84e214efb22311b8c9bf3d6e8dde53ce4
Dier Cohen <i.am.dier.cohen@gmail.com>
2025-10-15 14:26:34 +0330

    add sortBy into Table context

--- src/components/Table/Table.context.tsx
+++ src/components/Table/Table.context.tsx
@@ -10,6 +10,8 @@ import type { DataType } from "../../data";
 interface ITableContextProps {
   query: string;
   setQuery: React.Dispatch<React.SetStateAction<string>>;
+  sortBy: string;
+  setSortBy: React.Dispatch<React.SetStateAction<string>>;
   allData: DataType[];
   setAllData: React.Dispatch<React.SetStateAction<DataType[]>>;
   page: number;
@@ -21,6 +23,8 @@ interface ITableContextProps {
 const TableContext = createContext<ITableContextProps>({
   query: "",
   setQuery: () => undefined,
+  sortBy: "",
+  setSortBy: () => undefined,
   allData: [],
   setAllData: () => undefined,
   page: 1,
@@ -32,9 +36,12 @@ const TableContext = createContext<ITableContextProps>({
 export const TableProvider: FC<PropsWithChildren> = ({ children }) => {
   const [allData, setAllData] = useState<DataType[]>([]);
   const [page, setPage] = useState<number>(1);
+  const [sortBy, setSortBy] = useState<string>("");
   const [totalCount, setTotalCount] = useState<number>(0);
   const [query, setQuery] = useState<string>("");
   const value: ITableContextProps = {
+    sortBy,
+    setSortBy,
     query,
     setQuery,
     setTotalCount,
